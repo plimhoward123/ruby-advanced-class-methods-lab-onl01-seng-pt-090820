aad39789806462eb5fc4ce530eea282ff927bc14
Plimon Howard V <plimon.howard@gmail.com>
2020-10-07 20:04:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(n)
+    s = self.new
+    s.name = n
+    return s
+  end
+
+  def self.create_by_name(n)
+    s = self.new
+    s.name = n
+    @@all << s
+    return s
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |value| value.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if (find_by_name(name) != nil)
+      return find_by_name(name)
+    else
+      create_by_name(name)
+    end
+  end
+  def self.alphabetical
+    tmparray = @@all.sort_by {|value| value.name}
+    return tmparray
+  end
+  def self.new_from_filename(str)
+    tmparry = str.split(/[-.]/)
+    s = self.new
+    s.artist_name = tmparry[0].delete_suffix(" ")
+    s.name = tmparry[1].delete_prefix(" ")
+    return s
+  end
+
+  def self.create_from_filename(str)
+    tmparry = str.split(/[-.]/)
+    s = self.new
+    s.artist_name = tmparry[0].delete_suffix(" ")
+    s.name = tmparry[1].delete_prefix(" ")
+    @@all << s
+
+  end
+  def self.destroy_all
+    self.all.clear
+  end
 end

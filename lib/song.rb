89c97adc9d44b8250299d20b8c92f4f653ead6ba
Plimon Howard V <plimon.howard@gmail.com>
2020-10-07 18:13:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,17 +20,6 @@ class Song
     @@all << s
   end
 
-  def self.find_by_name(name)
-    @@all.each do |val|
-      if(val.name == name)
-        return name
-      else
-        return false
-      end
-    end
-
-  end
-
   def self.destroy_all
     @@all = []
     return @@all
